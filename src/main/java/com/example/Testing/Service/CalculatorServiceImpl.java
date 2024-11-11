47211e3ddf74ace28e7fb6c0cb0e78c0641fbbc3
DiplineRU <artgul52@gmail.com>
2024-11-11 16:12:38 +0300

    complete work

--- src/main/java/com/example/Testing/Service/CalculatorServiceImpl.java
+++ src/main/java/com/example/Testing/Service/CalculatorServiceImpl.java
@@ -13,10 +13,13 @@ public class CalculatorServiceImpl {
     }
 
     public String multiply(int number1, int number2) {
-        return number1 + " x " + number2 + " = " + (number1+number2);
+        return number1 + " x " + number2 + " = " + (number1*number2);
     }
 
     public String divide(int number1, int number2) {
+        if (number2 == 0) {
+            throw new IllegalArgumentException("На ноль делить нельзя");
+        }
         return number1 + " / " + number2 + " = " + (number1/number2);
     }
 }
